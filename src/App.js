27fdb7ce5379b041eab40012f83b90c3ca091a4a
Yehor <95246054+whosyehor@users.noreply.github.com>
2024-05-20 18:38:49 +0200

    check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import './App.css'
+import Navbar from './Components/Navbar'
+import Home from './Components/pages/Home'
+import Products from './Components/pages/Products'
+import Services from './Components/pages/Services'
+import SignUp from './Components/pages/SignUp'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Navbar />
+			<div>
+				<Routes>
+					<Route path='/' element={<Home />}></Route>
+					<Route path='/services' element={<Services />}></Route>
+					<Route path='/products' element={<Products />}></Route>
+					<Route path='/sign-up' element={<SignUp />}></Route>
+				</Routes>
+			</div>
+		</Router>
+	)
 }
 
-export default App;
+export default App
